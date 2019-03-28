541907a10d7cd58a5e1c5ba3e0fd224c952cbb57
ardeshir <ardeshir.org@gmail.com>
2019-03-28 13:24:12 -0500

    adding templates

--- main.go
+++ main.go
@@ -4,11 +4,14 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"html/template"
+	"path/filepath"
 	"os"
 )
 
 var (
 	listenAddr = flag.String("addr", getenvWithDefault("LISTENADDR", ":8080"), "HTTP address to listen on")
+	tmpl = template.New("")
 )
 
 func getenvWithDefault(name, defaultValue string) string {
@@ -20,9 +23,22 @@ func getenvWithDefault(name, defaultValue string) string {
 	return val
 }
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	tmpl.ExecuteTemplate(w, "index.html", nil)
+}
+
+
 func main() {
 	flag.Parse()
 
+	var err error
+	_, err = tmpl.ParseGlob(filepath.Join(".", "templates","*.html"))
+	if err != nil {
+		log.Fatalf("Unable to parse templates: %v\n", err)
+	}
+
 	log.Printf("Listening on %s\n", *listenAddr)
+
+	http.HandleFunc("/", handler)
 	http.ListenAndServe(*listenAddr, nil)
 }
